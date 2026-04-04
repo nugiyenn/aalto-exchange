d0af8c57a71ae1d78cf79feef4baa909d584b15c
Andy Nguyen <nguyenandy21@hotmail.com>
2026-04-04 18:27:13 +0300

    feat: make leaflet map more colorful using standard OpenStreetMap tiles

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -27,8 +27,8 @@ export default function Map() {
       className="h-full w-full bg-sky-50 z-0 relative"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {filteredUniversities.map((uni, idx) => {
--- src/components/MiniMap.tsx
+++ src/components/MiniMap.tsx
@@ -30,8 +30,8 @@ export default function MiniMap({ lat, lng, name }: { lat: number; lng: number; 
       className="h-full w-full z-0 relative"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[lat, lng]} icon={customIcon}>
         <Popup className="text-slate-900 font-medium text-xs">
